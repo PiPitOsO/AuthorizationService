de4c6202ee1761390a8c7f52805fb4883fd11964
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2021-07-04 22:00:28 +0300

    containers 0.1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:8-jdk-alpine
 
-EXPOSE 8080
+EXPOSE 8081
 
-ADD target/maven-archiver/Authorizationservice-0.0.1-SNAPSHOT.jar myapp.jar
+ADD target/Authorizationservice-0.0.1-SNAPSHOT.jar myapp.jar
 
 ENTRYPOINT ["java", "-jar", "/myapp.jar"]
--- src/test/java/i/ru/authorizationservice/AuthorizationServiceApplicationTests.java
+++ src/test/java/i/ru/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package i.ru.authorizationservice;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthorizationServiceApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    @Container
+    public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+//    @BeforeAll
+//    public static void setUp() {
+//        devapp.start();
+//        prodapp.start();
+//    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> forEntity1 = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
+        ResponseEntity<String> forEntity2 = restTemplate.getForEntity("http://localhost:" + prodapp.getMappedPort(8081), String.class);
+        System.out.println(forEntity1.getBody());
+        System.out.println(forEntity2.getBody());
     }
-
 }
